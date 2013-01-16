4f0e5bb8cb2a2025604e6fb16e029e5e590fb984
Alexey Klokov <Alexey.Klokov@gmail.com>
2013-01-16 15:13:46 +0400

    first version with example

---
+++ ParallaxUINavigatorController/ParallaxUINavigatorController/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ParallaxUINavigatorController
+//
+//  Created by Alexey Klokov on 1/16/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ParallaxUINavigatorController/ParallaxUINavigatorController/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ParallaxUINavigatorController
+//
+//  Created by MacMini US2 on 1/16/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
